8a92e0af8e9458e29a4fcc498fea6f0824ccef70
berglindoma13 <berglindis@gmail.com>
2022-09-01 13:14:17 +0000

    cors added

--- index.ts
+++ index.ts
@@ -1,13 +1,27 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import { routes } from './routes'
+import cors from 'cors'
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.use('/', routes)
+var whitelist = ['http://localhost:3000', 'https://vistbokserver.herokuapp.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// app.use(cors(corsOptions))
+
+app.use('/', routes, cors(corsOptions))
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
