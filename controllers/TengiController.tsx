c715437fe5ec646f153bd15bd1a2fbb723573d89
mariaomarsd <42963245+mariaomarsd@users.noreply.github.com>
2022-09-11 14:12:45 +0200

    tengi reddy

--- controllers/TengiController.tsx
+++ controllers/TengiController.tsx
@@ -15,105 +15,193 @@ import { DeleteAllProductsByCompany,
         GetUniqueProduct,
         GetAllProductsByCompanyid } from '../helpers/PrismaHelper'
 
-// TENGI COMPANY ID = 4
+// TENGI COMPANY ID = 3
 
 const TengiAPI = "https://api.integrator.is/Products/GetMany/?CompanyId=608c19f3591c2b328096b230&ApiKey=b3a6e86d4d4d6612b55d436f7fa60c65d0f8f217c34ead6333407162d308982b&Status=2&Brands=61efc9d1591c275358c86f84" 
 
-var updatedProducts = [];
-var createdProducts = [];
-var productsNotValid = [];
+var updatedProducts: Array<DatabseProduct> = [];
+var createdProducts: Array<DatabseProduct> = [];
+var productsNotValid: Array<DatabseProduct> = [];
 
 export const InsertAllTengiProducts = async(req, res) => {
-    const tengiData = await requestTengiApi();  
-    //process all data and insert into database
-    await ProcessForDatabase(tengiData)
-    return res.end("We made it! And it's great");
+  const tengiData = await requestTengiApi();  
+  //process all data and insert into database
+  await GetProducts(tengiData)
+  return res.end("We made it! And it's great");
 };
 
 export const DeleteAllTengiProducts = async(req,res) => {
-    // delete all products with company id 4
-    DeleteAllProductsByCompany(4)
-    res.end("All Tengi products deleted")
+  // delete all products with company id 3
+  DeleteAllProductsByCompany(3)
+  res.end("All Tengi products deleted")
 }
 
 
 export const DeleteAllTengiCert = async(req,res) => {
-    // delete all product certificates connected to company id 4
-    DeleteAllCertByCompany(4)
-    res.end("all product certificates deleted for Tengi")
-  }
+  // delete all product certificates connected to company id 3
+  DeleteAllCertByCompany(3)
+  res.end("all product certificates deleted for Tengi")
+}
 
 const requestTengiApi = async() => {
-  return axios.get(TengiAPI).then(response => {
-    if (response.status === 200) {
-        // breyta úr any í tengiResponseData
-        const data : any = response;
-        // console.log('DATA', data.data)
-        return data.data;
-    }else{
-      console.log(`Error occured : ${response.status} - ${response.statusText}`);
-    } 
-  });
+return axios.get(TengiAPI).then(response => {
+  if (response.status === 200) {
+    const data = response;
+    return data.data;
+  }else{
+    console.log(`Error occured : ${response.status} - ${response.statusText}`);
+  } 
+});
 }
 
 // const getcat = async(data) => {
-//   var categories = []
-//   data.map(prod => {
-//     // console.log('prod', prod.fl)
-//     prod.fl.map(cat => {
-//       // console.log('cat', cat.Name)
-//       if(!categories.includes(cat.Name)) {
-//         categories.push(cat.Name)
-//       }
-//     })
+// var categories = []
+// data.map(prod => {
+//   // console.log('prod', prod.fl)
+//   prod.fl.map(cat => {
+//     // console.log('cat', cat.Name)
+//     if(!categories.includes(cat.Name)) {
+//       categories.push(cat.Name)
+//     }
 //   })
-//   console.log(categories)
+// })
+// console.log(categories)
 // }
 
-// // WRITE FILES MISSING HERE
+// WRITE FILES MISSING HERE
 
-// const next = async(data) => {
-//   // console.log('DATA', data)
-//   // data.map( => {
-//   //   // console.log('her', af.fl)
-//   //   getcat(data)
-//   // })
-//   getcat(data)
-// }
 
-const ProcessForDatabase = async(data) => {
-  // console.log("DATA NUNA",data.Data)
-  const allprod : Array<TestControllerProduct> = [];
-  data.Data.map(prod => {
-    // console.log('PROD', prod.Attachments)
-    var temp_prod : TestControllerProduct = {
-      id: prod.Id,
-      prodName: prod.StandardFields.Name,
-      longDescription: prod.StandardFields.Description,
-      shortDescription: prod.StandardFields.ShortDescription,
-      fl: prod.StandardFields.Categories,
-      prodImage: prod.Images[0].Url,
-      url: 'vantar',
-      brand: prod.StandardFields.Brands,
-      fscUrl: "vantar",
-      epdUrl: "vantar",
-      vocUrl: "vantar",
-      ceUrl: "vantar",
-      certificates: [
-          { name: "fsc", val:  "FALSE" },
-          { name: "epd", val:  "FALSE" },
-          { name: "voc", val:  "FALSE" },
-          { name: "sv_allowed", val:  "FALSE" },
-          { name: "sv", val:  "FALSE" },
-          { name: "breeam", val:  "FALSE" },
-          { name: "blengill", val:  "FALSE" },
-          { name: "ev", val: "FALSE" },
-          { name: "ce", val: "TRUE" }
-      ]
+const GetProducts = async(data) => {
+// console.log("DATA NUNA",data.Data)
+const allprod : Array<DatabseProduct> = [];
+data.Data.map(prod => {
+  // console.log('PROD', prod.StandardFields.Categories[1].Name)
+  const temp_prod : DatabseProduct = {
+    id: prod.StandardFields.SKU,
+    prodName: prod.StandardFields.Name,
+    longDescription: prod.StandardFields.Description,
+    shortDescription: prod.StandardFields.ShortDescription,
+    fl: prod.StandardFields.Categories[1].Name, // Breyta nuna er allt að fá child nr 1 sem flokk
+    prodImage: prod.Images[0].Url,
+    url: 'https://www.tengi.is/', // EKKI HÆGT AÐ SKOÐA VÖRU Á VEF ÞVÍ ÞAÐ ER EKKI LINKUR
+    brand: prod.StandardFields.Brands[0].Name,
+    fscUrl: "vantar",
+    epdUrl: "vantar",
+    vocUrl: "vantar",
+    ceUrl: "vantar",
+    certificates: [
+      'TRUE'  === 'TRUE' ? { name: "SV_ALLOWED"} : null,
+    ].filter(cert => cert !== null)
+  }
+  allprod.push(temp_prod)
+})
+// console.log('ALL prods', allprod)
+ProcessForDatabase(allprod)
+}
 
+const UpsertProductInDatabase = async(product : DatabseProduct, approved : boolean, create : boolean, certChange : boolean) => {
+// get all product certificates from sheets
+
+// const convertedCertificates: Array<DatabaseCertificate> = product.certificates.map(certificate => { if(certificate.val=="TRUE") {return {name: certificate.name.toUpperCase() }} })
+// Object.keys(convertedCertificates).forEach(key => convertedCertificates[key] === undefined && delete convertedCertificates[key]);
+const validatedCertificates = CertificateValidator({ certificates: product.certificates })
+
+if(validatedCertificates.length === 0){
+  // no valid certificates for this product
+  productsNotValid.push(product)
+  return;
+}
+
+if(create === true) {
+  if (validatedCertificates.length !== 0) {
+    if (product.id !== "") {
+      createdProducts.push(product)
+      // check valid date when product is created
+      var validDate = await ValidDate(validatedCertificates, product)
     }
-    allprod.push(temp_prod)
+  }
+}
+if(certChange === true) {
+  //delete all productcertificates so they wont be duplicated and so they are up to date
+  DeleteProductCertificates(product.id)
+  if(validatedCertificates.length !== 0 ) {
+    if (product.id !== "") {
+      updatedProducts.push(product)
+      // check valid date when the certificates have changed
+      var validDate = await ValidDate(validatedCertificates, product)
+    }
+  }
+}
+// update or create product in database if the product has a productnumber (vörunúmer)
+if(product.id !== "") {
+  await UpsertProduct(product, approved, 3)
+  if(certChange === true || create === true) {
+    console.log('og at last komst hingað')
+    await CreateProductCertificates(product, validDate, validatedCertificates)
+  }
+}
+}
+
+const isProductListFound = async(products : Array<TestControllerProduct>) => {
+// get all current products from this company
+const currprods = await GetAllProductsByCompanyid(4)
+const nolonger = currprods.map((curr_prod) => {
+  const match = products.map((prod) => {
+    if(curr_prod.productid == prod.id) {
+      return true
+    }
   })
-  // console.log('ALL prods', allprod)
-//   next(allprod)
+  if(!match.includes(true)) {
+    return curr_prod
+  }
+}).filter(item=>item!==undefined)
+// productsNoLongerComingInWriteFile(nolonger)
+// deleta prodcut from prisma database
+nolonger.map(product => {
+  DeleteProduct(product.productid)
+})
+}
+
+const ProcessForDatabase = async(products : Array<DatabseProduct>) => {
+// check if product is in database but not coming in from company anymore
+// isProductListFound(products)
+
+products.map(async(product) => {
+  const prod = await GetUniqueProduct(product.id)
+  var approved = null;
+  if (prod !== null){
+    approved = prod.approved;
+    var certChange : boolean = false;
+    prod.certificates.map((cert) => {
+      if (cert.certificateid == 1) {
+        // epd file url is not the same
+        if(cert.fileurl !== product.epdUrl) {
+            certChange = true;
+            approved = false;
+        }
+      }
+      if (cert.certificateid == 2) {
+        // fsc file url is not the same
+        if(cert.fileurl !== product.fscUrl) {
+            certChange = true;
+            approved = false;
+        }
+      }
+      if (cert.certificateid == 3) {
+        // voc file url is not the same
+        if(cert.fileurl !== product.vocUrl) {
+            certChange = true;
+            approved = false;
+        }
+      }
+    })
+  }
+  else {
+    var create = true;
+    var certChange = false;
+  }
+  UpsertProductInDatabase(product, approved, create, certChange)
+})
+// write all appropriate files
+// WriteAllFiles()
 }
--- dist/controllers/TengiController.js
+++ dist/controllers/TengiController.js
@@ -14,8 +14,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DeleteAllTengiCert = exports.DeleteAllTengiProducts = exports.InsertAllTengiProducts = void 0;
 const axios_1 = __importDefault(require("axios"));
+const CertificateValidator_1 = require("../helpers/CertificateValidator");
+const ValidDate_1 = require("../helpers/ValidDate");
+const CreateProductCertificates_1 = require("../helpers/CreateProductCertificates");
 const PrismaHelper_1 = require("../helpers/PrismaHelper");
-// TENGI COMPANY ID = 4
+// TENGI COMPANY ID = 3
 const TengiAPI = "https://api.integrator.is/Products/GetMany/?CompanyId=608c19f3591c2b328096b230&ApiKey=b3a6e86d4d4d6612b55d436f7fa60c65d0f8f217c34ead6333407162d308982b&Status=2&Brands=61efc9d1591c275358c86f84";
 var updatedProducts = [];
 var createdProducts = [];
@@ -23,28 +26,26 @@ var productsNotValid = [];
 const InsertAllTengiProducts = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const tengiData = yield requestTengiApi();
     //process all data and insert into database
-    yield ProcessForDatabase(tengiData);
+    yield GetProducts(tengiData);
     return res.end("We made it! And it's great");
 });
 exports.InsertAllTengiProducts = InsertAllTengiProducts;
 const DeleteAllTengiProducts = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    // delete all products with company id 4
-    (0, PrismaHelper_1.DeleteAllProductsByCompany)(4);
+    // delete all products with company id 3
+    (0, PrismaHelper_1.DeleteAllProductsByCompany)(3);
     res.end("All Tengi products deleted");
 });
 exports.DeleteAllTengiProducts = DeleteAllTengiProducts;
 const DeleteAllTengiCert = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    // delete all product certificates connected to company id 4
-    (0, PrismaHelper_1.DeleteAllCertByCompany)(4);
+    // delete all product certificates connected to company id 3
+    (0, PrismaHelper_1.DeleteAllCertByCompany)(3);
     res.end("all product certificates deleted for Tengi");
 });
 exports.DeleteAllTengiCert = DeleteAllTengiCert;
 const requestTengiApi = () => __awaiter(void 0, void 0, void 0, function* () {
     return axios_1.default.get(TengiAPI).then(response => {
         if (response.status === 200) {
-            // breyta úr any í tengiResponseData
             const data = response;
-            // console.log('DATA', data.data)
             return data.data;
         }
         else {
@@ -53,59 +54,143 @@ const requestTengiApi = () => __awaiter(void 0, void 0, void 0, function* () {
     });
 });
 // const getcat = async(data) => {
-//   var categories = []
-//   data.map(prod => {
-//     // console.log('prod', prod.fl)
-//     prod.fl.map(cat => {
-//       // console.log('cat', cat.Name)
-//       if(!categories.includes(cat.Name)) {
-//         categories.push(cat.Name)
-//       }
-//     })
+// var categories = []
+// data.map(prod => {
+//   // console.log('prod', prod.fl)
+//   prod.fl.map(cat => {
+//     // console.log('cat', cat.Name)
+//     if(!categories.includes(cat.Name)) {
+//       categories.push(cat.Name)
+//     }
 //   })
-//   console.log(categories)
+// })
+// console.log(categories)
 // }
-// // WRITE FILES MISSING HERE
-// const next = async(data) => {
-//   // console.log('DATA', data)
-//   // data.map( => {
-//   //   // console.log('her', af.fl)
-//   //   getcat(data)
-//   // })
-//   getcat(data)
-// }
-const ProcessForDatabase = (data) => __awaiter(void 0, void 0, void 0, function* () {
+// WRITE FILES MISSING HERE
+const GetProducts = (data) => __awaiter(void 0, void 0, void 0, function* () {
     // console.log("DATA NUNA",data.Data)
     const allprod = [];
     data.Data.map(prod => {
-        // console.log('PROD', prod.Attachments)
-        var temp_prod = {
-            id: prod.Id,
+        // console.log('PROD', prod.StandardFields.Categories[1].Name)
+        const temp_prod = {
+            id: prod.StandardFields.SKU,
             prodName: prod.StandardFields.Name,
             longDescription: prod.StandardFields.Description,
             shortDescription: prod.StandardFields.ShortDescription,
-            fl: prod.StandardFields.Categories,
+            fl: prod.StandardFields.Categories[1].Name,
             prodImage: prod.Images[0].Url,
             url: 'vantar',
-            brand: prod.StandardFields.Brands,
+            brand: prod.StandardFields.Brands[0].Name,
             fscUrl: "vantar",
             epdUrl: "vantar",
             vocUrl: "vantar",
             ceUrl: "vantar",
             certificates: [
-                { name: "fsc", val: "FALSE" },
-                { name: "epd", val: "FALSE" },
-                { name: "voc", val: "FALSE" },
-                { name: "sv_allowed", val: "FALSE" },
-                { name: "sv", val: "FALSE" },
-                { name: "breeam", val: "FALSE" },
-                { name: "blengill", val: "FALSE" },
-                { name: "ev", val: "FALSE" },
-                { name: "ce", val: "TRUE" }
-            ]
+                'TRUE' === 'TRUE' ? { name: "SV_ALLOWED" } : null,
+            ].filter(cert => cert !== null)
         };
         allprod.push(temp_prod);
     });
     // console.log('ALL prods', allprod)
-    //   next(allprod)
+    ProcessForDatabase(allprod);
+});
+const UpsertProductInDatabase = (product, approved, create, certChange) => __awaiter(void 0, void 0, void 0, function* () {
+    // get all product certificates from sheets
+    // const convertedCertificates: Array<DatabaseCertificate> = product.certificates.map(certificate => { if(certificate.val=="TRUE") {return {name: certificate.name.toUpperCase() }} })
+    // Object.keys(convertedCertificates).forEach(key => convertedCertificates[key] === undefined && delete convertedCertificates[key]);
+    const validatedCertificates = (0, CertificateValidator_1.CertificateValidator)({ certificates: product.certificates });
+    if (validatedCertificates.length === 0) {
+        // no valid certificates for this product
+        productsNotValid.push(product);
+        return;
+    }
+    if (create === true) {
+        if (validatedCertificates.length !== 0) {
+            if (product.id !== "") {
+                createdProducts.push(product);
+                // check valid date when product is created
+                var validDate = yield (0, ValidDate_1.ValidDate)(validatedCertificates, product);
+            }
+        }
+    }
+    if (certChange === true) {
+        //delete all productcertificates so they wont be duplicated and so they are up to date
+        (0, PrismaHelper_1.DeleteProductCertificates)(product.id);
+        if (validatedCertificates.length !== 0) {
+            if (product.id !== "") {
+                updatedProducts.push(product);
+                // check valid date when the certificates have changed
+                var validDate = yield (0, ValidDate_1.ValidDate)(validatedCertificates, product);
+            }
+        }
+    }
+    // update or create product in database if the product has a productnumber (vörunúmer)
+    if (product.id !== "") {
+        yield (0, PrismaHelper_1.UpsertProduct)(product, approved, 3);
+        if (certChange === true || create === true) {
+            console.log('og at last komst hingað');
+            yield (0, CreateProductCertificates_1.CreateProductCertificates)(product, validDate, validatedCertificates);
+        }
+    }
+});
+const isProductListFound = (products) => __awaiter(void 0, void 0, void 0, function* () {
+    // get all current products from this company
+    const currprods = yield (0, PrismaHelper_1.GetAllProductsByCompanyid)(4);
+    const nolonger = currprods.map((curr_prod) => {
+        const match = products.map((prod) => {
+            if (curr_prod.productid == prod.id) {
+                return true;
+            }
+        });
+        if (!match.includes(true)) {
+            return curr_prod;
+        }
+    }).filter(item => item !== undefined);
+    // productsNoLongerComingInWriteFile(nolonger)
+    // deleta prodcut from prisma database
+    nolonger.map(product => {
+        (0, PrismaHelper_1.DeleteProduct)(product.productid);
+    });
+});
+const ProcessForDatabase = (products) => __awaiter(void 0, void 0, void 0, function* () {
+    // check if product is in database but not coming in from company anymore
+    // isProductListFound(products)
+    products.map((product) => __awaiter(void 0, void 0, void 0, function* () {
+        const prod = yield (0, PrismaHelper_1.GetUniqueProduct)(product.id);
+        var approved = null;
+        if (prod !== null) {
+            approved = prod.approved;
+            var certChange = false;
+            prod.certificates.map((cert) => {
+                if (cert.certificateid == 1) {
+                    // epd file url is not the same
+                    if (cert.fileurl !== product.epdUrl) {
+                        certChange = true;
+                        approved = false;
+                    }
+                }
+                if (cert.certificateid == 2) {
+                    // fsc file url is not the same
+                    if (cert.fileurl !== product.fscUrl) {
+                        certChange = true;
+                        approved = false;
+                    }
+                }
+                if (cert.certificateid == 3) {
+                    // voc file url is not the same
+                    if (cert.fileurl !== product.vocUrl) {
+                        certChange = true;
+                        approved = false;
+                    }
+                }
+            });
+        }
+        else {
+            var create = true;
+            var certChange = false;
+        }
+        UpsertProductInDatabase(product, approved, create, certChange);
+    }));
+    // write all appropriate files
+    // WriteAllFiles()
 });
