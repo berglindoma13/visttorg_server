d049027a0e3a03728e56df6062abdf025be427db
mariaomarsd <42963245+mariaomarsd@users.noreply.github.com>
2023-05-06 18:13:52 +0200

    update and delete user project, controller and route

--- controllers/ProjectsController.tsx
+++ controllers/ProjectsController.tsx
@@ -4,11 +4,14 @@ import prismaInstance from '../lib/prisma';
 
 export const AddProject = async(req: Request, res: Response) => {
 
-  const { title, address, country, status, ownerEmail } = req.body.data
+  const { title, certificatesystem, address, country, status, ownerEmail } = req.body.data
+
+//   console.log("certificite system", certificatesystem)
 
     await prismaInstance.vistbokProject.create({
         data:{
             title: title,
+            certificatesystem: certificatesystem,
             address: address,
             country: country,
             status: status,
@@ -17,6 +20,41 @@ export const AddProject = async(req: Request, res: Response) => {
     })
 
     console.log('created');
+};
+
+
+export const UpdateProject = async(req: Request, res: Response) => {
+
+    const { oldTitle, title, certificatesystem, address, country, status, ownerEmail } = req.body.data
+  
+    // console.log("req boy data", req.body.data )
+  
+      await prismaInstance.vistbokProject.update({
+            where: { projectIdentifier : { title: oldTitle, ownerEmail: ownerEmail }},
+            data:{
+                title: title,
+                certificatesystem: certificatesystem,
+                address: address,
+                country: country,
+                status: status,
+                ownerEmail: ownerEmail
+            }
+        })
+  
+      console.log('updated');
+};
+
+export const DeleteProject = async(req: Request, res: Response) => {
+
+    const { title, ownerEmail } = req.body.data
+  
+    // console.log("req boy data", req.body.data )
+  
+      await prismaInstance.vistbokProject.delete({
+            where: { projectIdentifier : { title: title, ownerEmail: ownerEmail }}
+        })
+  
+      console.log('deleted');
 };
 
 export const GetProject = async(req: Request, res: Response) => {
